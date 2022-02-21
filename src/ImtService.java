985b875ed3cbfd3a3b32d9cde7e3761f93619f65
Scriper.irk <scriper.irk@gmail.com>
2022-02-21 21:42:05 +0800

    initial commit

--- src/ImtService.java
+++ src/ImtService.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class ImtService {
+    public double calculate(int wt, double height){
+        double height2 = height * 2;
+        return wt / height2;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        ImtService service = new ImtService();
+        double imt = service.calculate(75, 1.72);
+        System.out.println("Индекс массы тела равен: " + imt + " (кг/м2)");
+    }
 }
