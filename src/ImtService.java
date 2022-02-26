75ed90cb176d8aad3a285e5b3db72460edc973d7
Scriper.irk <scriper.irk@gmail.com>
2022-02-27 01:18:22 +0800

    initial commit

--- src/ImtService.java
+++ src/ImtService.java
@@ -1,5 +1,5 @@
 public class ImtService {
-    public double calculate(int wt, double height){
+    public double calculate(int wt, double height) {
         double height2 = height * 2;
         return wt / height2;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        ImtService service = new ImtService();
+        ImtService service;
+        service = new ImtService();
         double imt = service.calculate(75, 1.72);
         System.out.println("Индекс массы тела равен: " + imt + " (кг/м2)");
     }
